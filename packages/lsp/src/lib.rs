41e891ab59dba13140e178e67ccb63ed74275c95
eveeify <eveeg1971@gmail.com>
2024-03-20 10:16:16 +1100

    feat: cargo tests for lsp init and made it cleaner

--- packages/lsp/src/lib.rs
+++ packages/lsp/src/lib.rs
@@ -1,10 +1,13 @@
-use std::{fmt::format, io::{BufRead, Read, Write}, process::{Command, Stdio}};
+use std::{io::{BufRead, Read, Write}, process::{Command, Stdio}};
 use serde_json::json;
 use anyhow::Result;
 
 pub struct Server {
     lspserver: String,
 }
+
+
+pub(crate) const JSONRPCVERSION: &str = "2.0";
 
 impl Server {
     pub fn new(file_type: &str) -> Self {
@@ -15,11 +18,10 @@ impl Server {
             "cpp" => "ccls".to_string(),
             _ => String::new(), 
         };
-        println!("LSP is Type is Set to: {} based on the filetype of {}", lspserver, file_type);
         Self { lspserver }
     }
 
-    pub fn start_lsp(&self, projectPath: &str) -> Result<()> {
+    pub fn start_lsp(&self, project_path: &str) -> Result<()> {
         let process = Command::new(&self.lspserver)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
@@ -28,13 +30,14 @@ impl Server {
         let mut stdin = process.stdin.unwrap();
         let stdout = process.stdout.unwrap();
 
+
         let init_request = json!({
-            "jsonrpc": "2.0",
+            "jsonrpc": JSONRPCVERSION,
             "id": 1,
             "method": "initialize",
             "params": {
                 "processId": null,
-                "rootUri": format!("file://{}", projectPath), 
+                "rootUri": format!("file://{}", project_path),
                 "capabilities": {
                     "workspaceFolders": true,
                     "textdocument": {
@@ -50,22 +53,40 @@ impl Server {
         let body = serde_json::to_string(&init_request)?;
         let header = format!("Content-Length: {}\r\n\r\n", body.len());
         let req = format!("{}{}", header, body);
-
+        
         stdin.write_all(req.as_bytes())?;
-
+        
         let mut reader = std::io::BufReader::new(stdout);
         let mut content_length = String::new();
         reader.read_line(&mut content_length)?;
-        let mut empty_line = String::new();
-        reader.read_line(&mut empty_line)?;
-
         let content_length = content_length.strip_prefix("Content-Length: ").unwrap().trim().parse::<usize>()?;
-
+        
         let mut body = vec![0; content_length];
         reader.read_exact(&mut body)?;
-
+        
         println!("{}", String::from_utf8_lossy(&body));
 
         Ok(())
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    use std::env;
+
+    use super::*;
+
+    #[test]
+    fn start_lsp() {
+        let lsp = Server::new("rs");
+
+
+        let current_dir = env::current_dir().expect("Failed to get current directory");
+        let current_dir_str = current_dir.to_str().expect("Failed to convert path to string");
+        let lsp_result = lsp.start_lsp(current_dir_str);
+        assert!(lsp_result.is_ok())
     }
 }
--- packages/lsp/src/main.rs
+++ packages/lsp/src/main.rs
@@ -7,5 +7,5 @@ fn main() {
 
     let current_dir = env::current_dir().expect("Failed to get current directory");
     let current_dir_str = current_dir.to_str().expect("Failed to convert path to string");
-    lsp.start_lsp("/Users/eveeify/Projects/Editify/Editify/packages/lsp").is_ok();
+    let _ = lsp.start_lsp(current_dir_str);
 }
