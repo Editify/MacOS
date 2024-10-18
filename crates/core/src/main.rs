949fcccb989df858da4259df6389e2661c820844
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-18 23:24:24 +1100

    feat: wgpu instance

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,10 +1,12 @@
+use wgpu::Backends;
+
 fn main() {
     let instances = wgpu::Instance::new(wgpu::InstanceDescriptor {
-        backends: wgpu::util::backend_bits_from_env()
-            .unwrap(wgpu::Backends::VULKAN | wgpu::Backends::Metal),
+        backends: Backends::METAL | Backends::VULKAN,
         dx12_shader_compiler: wgpu::util::dx12_shader_compiler_from_env().unwrap_or_default(),
         gles_minor_version: wgpu::Gles3MinorVersion::Automatic,
         flags: wgpu::InstanceFlags::default(),
     });
-    print("Instance {}", instances);
+
+    println!("Instance {:?}", instances);
 }
