f9a51fc55815b7d3b0382a6feeb9d3bcc7c479cf
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-18 22:59:45 +1100

    fix: rust errors

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,8 +1,10 @@
-use wgpu::Backends::{METAL, VULKAN};
-
 fn main() {
-    let instances = wgpu::Instance::new(wpgu::InstanceDescriptor {
+    let instances = wgpu::Instance::new(wgpu::InstanceDescriptor {
         backends: wgpu::util::backend_bits_from_env()
-            .unwrap(wgpu::Backends::VULKAN | wgpu::Backends::Metal)
-    })
+            .unwrap(wgpu::Backends::VULKAN | wgpu::Backends::Metal),
+        dx12_shader_compiler: wgpu::util::dx12_shader_compiler_from_env().unwrap_or_default(),
+        gles_minor_version: wgpu::Gles3MinorVersion::Automatic,
+        flags: wgpu::InstanceFlags::default(),
+    });
+    print("Instance {}", instances);
 }
