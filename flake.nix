5db42d66721c9eb186b7fd3d06604bdc80c11755
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2024-10-27 20:17:40 +1100

    feat: editify zig init

--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = inputs@{ nixpkgs, flake-parts, ... }:
@@ -26,19 +22,11 @@
           ];
         };
 
-        devShells.default = with pkgs; let 
-        toolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = ["rust-src"];
-          targets = ["x86_64-unknown-linux-gnu"];
-        };
-
-        in mkShell
+        devShells.default = pkgs.mkShell
         {
           nativeBuildInputs = with pkgs; [
-            toolchain
-						openssl
+						zig
           ] ++ lib.optionals stdenv.isLinux [
-            # Additional Dependencies here for Linux.
           ] ++ lib.optionals stdenv.isDarwin (with pkgs; [
             darwin.apple_sdk.Frameworks.Metal
           ]);
